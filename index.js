010c4407a5c6a596c36260a9395c97ee5f7f9f3c
Mark Amirkan <markdamirkan@gmail.com>
2020-03-16 20:59:17 -0500

    rewrote functions of the code

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = {
     encode: function (string) {
-        let encodedString = btoa(string);
+        let encodedString = Buffer.from(string).toString('base64');
         return encodedString;
     },
     decode: function (string) {
-        let decodedString = atob(string);
+        let decodedString = Buffer.from(string, 'base64').toString();
         return decodedString;
     }
 }
